972aedce86d7d886d269f8a8614c7e3dab4438d5
HARSHSINGH0 <hs475052@gmail.com>
2022-07-29 23:20:42 +0530

    Revert "added some stuff"
    This reverts commit 2602a71422f1d826166b839f0b88611fb8b8fc3d.

--- WonderCook/Components/FilteredRecipesViewComponent.cs
+++ WonderCook/Components/FilteredRecipesViewComponent.cs
@@ -4,18 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
-using WonderCook.Models;
+
 namespace WonderCook.Components
 {
     public class FilteredRecipesViewComponent :ViewComponent
     {
-        //private Recipes _recipes;
-        //public async Task<IViewComponentResult> InvokeAsync() {
-        //    var recipe= await _recipes
-        //    return View();
-        //}
-        //public async Task<List<WonderCook.Models.Recipes>> GetFilteredRecipe() { 
-        //    return await         
-        //}
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            //var recipe= await 
+            return View();
+        }
     }
 }
