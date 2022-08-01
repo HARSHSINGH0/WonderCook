4d131acb4ffb997e395c98819046e7be929c0d90
HARSHSINGH0 <hs475052@gmail.com>
2022-08-01 23:28:29 +0530

    fixed erros in preview

--- WonderCook/Content/javascript/sideview2.js
+++ WonderCook/Content/javascript/sideview2.js
@@ -30,14 +30,14 @@ function IngredientAndColor(e, btn, color, objButton) {
     var target = e.target, count = +target.dataset.count;
     target.style.backgroundColor = count === 1 ? "#ff8c00" : '#ffffff';
     target.dataset.count = count === 1 ? 0 : 1;
-    if (target.dataset.count == 0) {
-        const index = AllIngredients.indexOf(String(objButton.value));
-        if (index > -1) { // only splice array when item is found
-            array.splice(index, 1); // 2nd parameter means remove one item only
-        }
-        // array = [2, 9]
-        console.log(AllIngredients);
-    }
+    //if (target.dataset.count == 0) {
+    //    const index = AllIngredients.indexOf(String(objButton.value));
+    //    if (index > -1) { // only splice array when item is found
+    //        AllIngredients.splice(index, 1); // 2nd parameter means remove one item only
+    //    }
+    //    // array = [2, 9]
+    //    console.log(AllIngredients);
+    //}
     sessionStorage.setItem("IngredientArray", JSON.stringify(AllIngredients));
 
 }
